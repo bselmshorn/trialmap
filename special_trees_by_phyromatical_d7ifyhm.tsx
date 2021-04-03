d289888b576810f10e2a87080c7da1821c193959
bselmshorn <42200174+bselmshorn@users.noreply.github.com>
2021-04-03 13:55:26 +0200

    update

---
+++ special_trees_by_phyromatical_d7ifyhm.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="special_trees_by_phyromatical_d7ifyhm" tilewidth="32" tileheight="32" tilecount="625" columns="25">
+ <image source="../../../Downloads/bw_taragonia_mix___special_trees_by_phyromatical_d7ifyhm.png" width="800" height="800"/>
+ <tile id="54">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
